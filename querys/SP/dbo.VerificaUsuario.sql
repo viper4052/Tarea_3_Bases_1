a59cbdc327a42ce14925663196eb6826b21c9b53
viper4052 <viper4052@hotmail.com>
2024-11-13 11:42:44 -0600

    SP que verifica tipo de usuario
    permite asignar si es usuario regular o administrador

---
+++ querys/SP/dbo.VerificaUsuario.sql
@@ -0,0 +1,47 @@
+﻿CREATE PROCEDURE [dbo].[VerificaUsuario]
+	@TipoDeUsuario INT OUTPUT
+	, @OutResultCode INT
+	, @InUsername VARCHAR(128)
+	, @IdUser INT 
+	
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+
+	SET @OutResultCode = 0; 
+
+	IF NOT EXISTS
+	(
+		SELECT Nombre FROM [dbo].[Usuarios]
+		WHERE Nombre = @InUsername
+	)
+	SELECT @IdUser = Id FROM [dbo].[Usuarios]
+	WHERE @InUsername = Nombre;
+	
+	SET @TipoDeUsuario = (SELECT IdTipoDeUsuario 
+						  FROM dbo.Usuarios 
+						  WHERE Nombre = @InUsername);
+	
+	RETURN @TipoDeUsuario
+
+	END TRY
+	BEGIN CATCH 
+
+	INSERT INTO dbo.DBError VALUES 
+		(
+            SUSER_SNAME(),
+            ERROR_NUMBER(),
+            ERROR_STATE(),
+            ERROR_SEVERITY(),
+            ERROR_LINE(),
+            ERROR_PROCEDURE(),
+            ERROR_MESSAGE(),
+            GETDATE()
+        );
+		SET @OutResulTCode = 50008;
+		
+
+	END CATCH 
+    SET NOCOUNT OFF;
+END;
