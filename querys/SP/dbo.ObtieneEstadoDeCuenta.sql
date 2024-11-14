6d7692810a21529ce50b6e256ec27b7504158d8e
viper4052 <viper4052@hotmail.com>
2024-11-13 22:08:29 -0600

    editado sp

---
+++ querys/SP/dbo.ObtieneEstadoDeCuenta.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[ObtieneEstadoDeCuenta]
+	@OutResultCode INT OUTPUT
+	, @InUsername VARCHAR(128)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET @OutResultCode = 0;
+	SELECT @OutResultCode AS OutResultCode;
+
+	BEGIN
+		SELECT * FROM [dbo].[VistaEstadoDeCuenta] E
+		INNER JOIN [dbo].[TarjetaCreditoMaestra] M
+		ON E.IdTCM = M.IdTarjeta
+	END
+
+	SET NOCOUNT OFF;
+END
+RETURN 0
