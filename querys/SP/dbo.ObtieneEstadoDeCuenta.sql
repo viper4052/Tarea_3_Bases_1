565eae8aea57f16789bbced31f5ef5ec20fdd35b
viper4052 <viper4052@hotmail.com>
2024-11-16 12:09:07 -0600

    cambios a SP del estado de cuenta

--- querys/SP/dbo.ObtieneEstadoDeCuenta.sql
+++ querys/SP/dbo.ObtieneEstadoDeCuenta.sql
@@ -5,12 +5,28 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 	SET @OutResultCode = 0;
+
+	DECLARE @IdTCM INT
+			, @IdABuscar INT;
+
+	SET @IdABuscar = (SELECT Id
+					  FROM [dbo].[Usuarios]
+					  WHERE Nombre = @InUsername)
+					  
+	SET @IdTCM = (SELECT IdTarjeta 
+			  	  FROM dbo.VistaTCM 
+			  	  WHERE IdTarjetaHabiente = @IdABuscar)
+
 	SELECT @OutResultCode AS OutResultCode;
 
 	BEGIN
-		SELECT * FROM [dbo].[VistaEstadoDeCuenta] E
-		INNER JOIN [dbo].[TarjetaCreditoMaestra] M
-		ON E.IdTCM = M.IdTarjeta
+	SELECT Fecha,
+		   Descripcion,
+		   Referencia,
+		   Monto,
+		   NuevoSaldo
+	FROM dbo.Movimientos
+	WHERE IdTarjetaCreditoMaestra = @IdTCM
 	END
 
 	SET NOCOUNT OFF;
